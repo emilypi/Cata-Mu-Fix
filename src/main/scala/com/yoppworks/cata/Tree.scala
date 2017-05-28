0091e635d71c99a54d3e5e828fa6d192a0e9c7fd
Emily Pillmore <emily.pillmore@yoppworks.com>
2017-05-28 11:32:00 -0400

    Add Tree + Tree fold for contrast with TreeF

---
+++ src/main/scala/com/yoppworks/cata/Tree.scala
@@ -0,0 +1,17 @@
+package com.yoppworks.cata
+
+/**
+  * Created by emilypi on 5/28/17.
+  */
+sealed abstract class Tree[+A]
+final case class Branch[+A](a: A, l: Tree[A], r: Tree[A]) extends Tree[A]
+final case object Tip extends Tree[Nothing]
+
+object Tree {
+
+  def fold[A, B](f: A => B => B => B)(z: B)(t: Tree[A]): B =
+    t match {
+      case Branch(a, l, r) => f(a)(fold(f)(z)(l))(fold(f)(z)(r))
+      case Tip => z
+    }
+}
