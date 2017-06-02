a6397de209b8563aad69c0c979593d46d43c018e
Emily Pillmore <emily.pillmore@yoppworks.com>
2017-06-02 19:44:18 -0400

    Refactor Hylo, add listF operation

--- Cata Mu Fix Pt. I/src/main/scala/com/yoppworks/cata/package.scala
+++ Cata Mu Fix Pt. I/src/main/scala/com/yoppworks/cata/package.scala
@@ -24,12 +24,17 @@ package object cata {
   def ana[F[_]: Functor, B](ψ: CoAlgebra[B, F])(b: B): Fix[F] =
     Fix(implicitly[Functor[F]].fmap(ana[F, B](ψ))(ψ(b)))
 
-  def hylo[F[_]: Functor, A, B](φ: Algebra[F, B])(ψ: CoAlgebra[A, F]): A => B =
-    (a: A) => φ(implicitly[Functor[F]].fmap(hylo[F, A, B](φ)(ψ))(ψ(a)))
 
-  /** Alias for Hylo in terms of cata φ ∘ ana ψ */
+  def hylo[F[_]: Functor, A, B](φ: Algebra[F, B])(ψ: CoAlgebra[A, F]): A => B =
+    cata[B, F](φ) _  ∘  ana[F, A](ψ) _
+
   def _hylo[F[_]: Functor, A, B](φ: Algebra[F, B])(ψ: CoAlgebra[A, F]): A => B =
-     cata[B, F](φ) _  ∘  ana[F, A](ψ) _
+    (a: A) => φ(implicitly[Functor[F]].fmap(_hylo[F, A, B](φ)(ψ))(ψ(a)))
+
+
+
+  /** Examples of ListF operations */
+  def sumF = cata[Int, ListF[Int, ?]]{ case Cons(h, t) => h + t; case NilF => 0 } _
 
   def countF = cata[Int, ListF[Int, ?]]{ case Cons(h, t) => 1 + t; case NilF => 0 } _
 
